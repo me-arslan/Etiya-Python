27403691365c0c7b31cc0badf72126450c289585
me-arslan <mehmet.emin.arslan@outlook.com>
2022-11-17 10:48:36 +0300

    Odev-1

---
+++ main.py
@@ -0,0 +1,56 @@
+print("Merhaba Etiya")
+# string =  metinsel  ifade
+text = "Bu bir metindir"
+print  (10)
+
+# integer  = tam sayı
+number  =  10
+print (number)
+
+# double,float,decimal =  ondalıklı sayı
+dnumber = 5.3
+print (dnumber)
+
+#! matematiksel ifadeler
+print (number + 5)
+print (number - 5)
+print (number * 5)
+print (number / 5)
+print (number % 3)  # 10 / 3 = 1 // Bölümünden kalanı verir (mod3)
+
+#boolean, bool =  true /  false
+isVerified  =  True
+
+# mantıksal operatörler  = her mantıksal  operatör boolean değer döner
+
+print (1 == 2)
+print (2 != 2)
+
+print (2 > 2)
+print (2 < 2)
+
+print (2 >= 2)
+print (2 <= 2)
+
+print (10%2==0)
+print (50 / 2 == 25)
+print (50 / 2 == 25.000)
+
+
+#! mantıksal oparatörler
+
+# string operasyonları
+
+text = "Merhaba Etiya"
+print (text.upper())
+print (text.lower())
+
+print (text.startswith("Mer"))
+print (text.endswith("Etiya"))
+
+name = "Halit"
+age = 23
+company = "Kodlamaio"
+
+#print (name + " " + age + " yaşında " + company + "'de çalışıyor")
+print(f"{name} {age} yaşında {company}'de çalışıyor")
---
+++ odev-1.py
@@ -0,0 +1,27 @@
+# // ODEV-1
+
+baslik = "Haberiniz Olsun"  #String
+vade = 12   #Integer
+faizOrani1 = 1.47   #Float
+faizOrani2 = 1.44
+faizOrani = 1.47
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(vade))
+
+mesaj = "Hoşgedin"
+musteriAdi = "Mehmet"
+musteriSoyadi = "Arslan"
+sonucMesaj = mesaj + " " + musteriAdi + " " + musteriSoyadi + "!"
+
+print(sonucMesaj)
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1+sayi2)
+
+print(sonucMesaj)
+
+
